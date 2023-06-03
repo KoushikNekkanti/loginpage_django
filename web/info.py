fe889031d81e68a49ea7a9896041f082a5f30723
KoushikNekkanti <110541440+KoushikNekkanti@users.noreply.github.com>
2023-06-03 20:28:57 +0530

    Update info.py

--- web/info.py
+++ web/info.py
@@ -1,6 +1,6 @@
  
 EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_HOST_USER = 'miniproject.cloudcomputing@outlook.com'
+EMAIL_HOST_USER = '#' #create your own outlook
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = '#'
 EMAIL_PORT =587
